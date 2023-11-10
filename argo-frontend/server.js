00a62f42704f38817c700485f281758bb921ee77
Gkemhcs <gudikotieswarmani@gmail.com>
2023-11-10 13:21:26 +0000

    added appset

--- argo-frontend/server.js
+++ argo-frontend/server.js
@@ -3,3 +3,17 @@ converter.convert("gs://helm-argo/dilse.mp4", "gs://helm-argo/dilse.mp3", functi
     if (err) throw err;
     console.log("done");
 });
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  annotations:
+    iam.gke.io/gcp-service-account: argocd-fleet-admin@helm-argo.iam.gserviceaccount.com
+  name: argocd-application-controller
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  annotations:
+    iam.gke.io/gcp-service-account: argocd-fleet-admin@$PROJECT_ID.iam.gserviceaccount.com
+  name: argocd-server
